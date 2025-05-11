45d7a31ae816b7d2a0c4233e6b11ec607c4fefe6
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-05-12 04:34:23 +0900

    환경세팅 완료
    기존 코드로 돌아가지 않은 점이 많아 수정된 부분 많음
    1. build.gradle - window인 경우 조건문 넣어 필요없는 작업 제거 (없어야 잘 돌아감)
    2. env 파일을 인식못해 dotenv 라이브러리 추가 및 application에 인식하는 코드 삽입
    3. application.yaml에 active:local 추가 (해야지 성공적으로 돌아갔었음)

--- src/main/java/com/odos/odos_server/Application.java
+++ src/main/java/com/odos/odos_server/Application.java
@@ -1,13 +1,34 @@
 package com.odos.odos_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
+
   public static void main(String[] args) {
+    // Application.java
+    Dotenv dotenv1 = Dotenv.configure()
+            .directory("env")
+            .filename("db.env")
+            .load();
+
+    Dotenv dotenv2 = Dotenv.configure()
+            .directory("env")
+            .filename("server.env")
+            .load();
+
+    // 두 개 병합해서 환경변수로 등록
+    Stream.concat(dotenv1.entries().stream(), dotenv2.entries().stream())
+            .forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
     SpringApplication.run(Application.class, args);
+
+
   }
 }
 
