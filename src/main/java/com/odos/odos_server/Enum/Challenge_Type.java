c4232157f19553ba8978b07fb37aa5f5b47b7a62
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-05-23 00:56:37 +0900

    challenge type 수정

--- src/main/java/com/odos/odos_server/Enum/Challenge_Type.java
+++ src/main/java/com/odos/odos_server/Enum/Challenge_Type.java
@@ -1,6 +1,6 @@
 package com.odos.odos_server.Enum;
 
 public enum Challenge_Type {
-  FIXED, // 고정형
-  FREESTYLE // 자유형
+  FIXED, // 고정형 나중에 personal, public, official은 다른 개념??
+  FLEXIBLE // 자유형
 }
