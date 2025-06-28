0361c890b8ef6f14b72ffd4e25620719e91ed3c0
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-06-28 13:26:47 +0900

    push for checkout

--- src/main/java/com/odos/odos_server/domain/diary/dto/DiaryResponseDTO.java
+++ src/main/java/com/odos/odos_server/domain/diary/dto/DiaryResponseDTO.java
@@ -1,10 +1,15 @@
 package com.odos.odos_server.domain.diary.dto;
 
 import com.odos.odos_server.domain.challenge.dto.ChallengeDto;
+import com.odos.odos_server.domain.challenge.entity.Challenge;
 import com.odos.odos_server.domain.common.dto.ImgDto;
 import com.odos.odos_server.domain.common.dto.LikesDto;
 import com.odos.odos_server.domain.diary.entity.Diary;
+import com.odos.odos_server.domain.diary.entity.DiaryGoal;
+import com.odos.odos_server.domain.diary.entity.DiaryImage;
+import com.odos.odos_server.domain.diary.entity.DiaryLike;
 import com.odos.odos_server.domain.member.dto.MemberDto;
+import com.odos.odos_server.domain.member.entity.Member;
 import java.util.List;
 
 public record DiaryResponseDTO(
@@ -18,7 +23,13 @@ public record DiaryResponseDTO(
     List<DiaryGoalDTO> goals) {
 
   /** Convert Diary entity to DiaryResponseDTO. */
-  public static DiaryResponseDTO from(Diary entity) {
+  public static DiaryResponseDTO from(
+      Diary entity,
+      DiaryGoal goal,
+      DiaryImage img,
+      DiaryLike like,
+      Challenge challenge,
+      Member member) {
     return null;
   }
   //    return new DiaryResponseDTO(
